20b12f4fc78181f03839d4e2e14866699395ab60
laupwing <laupwing@gmail.com>
2025-02-02 18:25:11 +0100

    refactor: update RecommendedProduct fragment structure for improved readability

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -236,36 +236,36 @@ const FEATURED_COLLECTION_QUERY = `#graphql
                 ...FeaturedCollection
             }
         }
-  }
+    }
 ` as const
 
 const RECOMMENDED_PRODUCTS_QUERY = `#graphql
-  fragment RecommendedProduct on Product {
-    id
-    title
-    handle
-    priceRange {
-      minVariantPrice {
-        amount
-        currencyCode
-      }
-    }
-    images(first: 1) {
-      nodes {
+    fragment RecommendedProduct on Product {
         id
-        url
-        altText
-        width
-        height
-      }
+        title
+        handle
+        priceRange {
+        minVariantPrice {
+            amount
+            currencyCode
+        }
+        }
+        images(first: 1) {
+        nodes {
+            id
+            url
+            altText
+            width
+            height
+        }
+        }
     }
-  }
-  query RecommendedProducts ($country: CountryCode, $language: LanguageCode)
-    @inContext(country: $country, language: $language) {
-    products(first: 4, sortKey: UPDATED_AT, reverse: true) {
-      nodes {
-        ...RecommendedProduct
-      }
+    query RecommendedProducts ($country: CountryCode, $language: LanguageCode)
+        @inContext(country: $country, language: $language) {
+            products(first: 4, sortKey: UPDATED_AT, reverse: true) {
+                nodes {
+                    ...RecommendedProduct
+                }
+            }
     }
-  }
 ` as const
