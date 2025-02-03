282af9977e518d3d4cb63f535cccadbf70b877df
laupwing <laupwing@gmail.com>
2025-02-03 18:54:39 +0100

    style: add rounded corners to Image component in article route

--- app/routes/blogs.$blogHandle.$articleHandle.tsx
+++ app/routes/blogs.$blogHandle.$articleHandle.tsx
@@ -73,7 +73,7 @@ export default function Article() {
 
             {image && (
                 <Image
-                    className="custom-container"
+                    className="custom-container rounded-2xl"
                     data={image}
                     sizes="90vw"
                     loading="eager"
