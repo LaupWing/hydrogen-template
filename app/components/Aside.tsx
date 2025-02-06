bc59eb7170075ea32b05011d31a454754988b867
laupwing <laupwing@gmail.com>
2025-02-06 11:14:09 +0100

    refactor: remove unused DrawerFooter and submit button from Aside component

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -62,9 +62,6 @@ export function Aside({
                     </DrawerClose>
                 </DrawerHeader>
                 <main>{children}</main>
-                <DrawerFooter>
-                    <button>Submit</button>
-                </DrawerFooter>
             </DrawerContent>
         </Drawer>
         // <div
