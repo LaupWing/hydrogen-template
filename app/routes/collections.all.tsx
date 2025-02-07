dc162369bccedfdb45afb61da5588498bbd59393
laupwing <laupwing@gmail.com>
2025-02-07 07:46:10 +0100

    refactor: update layout and styling for Collections and ProductItem components

--- app/routes/collections.all.tsx
+++ app/routes/collections.all.tsx
@@ -52,8 +52,13 @@ export default function Collection() {
 
     return (
         <div className="bg-white py-16">
-            <h1>Products</h1>
-            <PaginatedResourceSection
+            <div className="custom-container">
+                {/* @ts-ignore */}
+                {products.nodes.map((product) => (
+                    <ProductItem key={product.id} product={product} />
+                ))}
+            </div>
+            {/* <PaginatedResourceSection
                 connection={products}
                 resourcesClassName="products-grid"
             >
@@ -64,7 +69,7 @@ export default function Collection() {
                         loading={index < 8 ? "eager" : undefined}
                     />
                 )}
-            </PaginatedResourceSection>
+            </PaginatedResourceSection> */}
         </div>
     )
 }
@@ -103,61 +108,61 @@ function ProductItem({
 }
 
 const PRODUCT_ITEM_FRAGMENT = `#graphql
-  fragment MoneyProductItem on MoneyV2 {
-    amount
-    currencyCode
-  }
-  fragment ProductItem on Product {
-    id
-    handle
-    title
-    featuredImage {
-      id
-      altText
-      url
-      width
-      height
+    fragment MoneyProductItem on MoneyV2 {
+        amount
+        currencyCode
     }
-    priceRange {
-      minVariantPrice {
-        ...MoneyProductItem
-      }
-      maxVariantPrice {
-        ...MoneyProductItem
-      }
-    }
-    variants(first: 1) {
-      nodes {
-        selectedOptions {
-          name
-          value
+    fragment ProductItem on Product {
+        id
+        handle
+        title
+        featuredImage {
+            id
+            altText
+            url
+            width
+            height
         }
-      }
+        priceRange {
+            minVariantPrice {
+                ...MoneyProductItem
+            }
+            maxVariantPrice {
+                ...MoneyProductItem
+            }
+        }
+        variants(first: 1) {
+            nodes {
+                selectedOptions {
+                    name
+                    value
+                }
+            }
+        }
     }
-  }
 ` as const
 
 // NOTE: https://shopify.dev/docs/api/storefront/2024-01/objects/product
 const CATALOG_QUERY = `#graphql
-  query Catalog(
-    $country: CountryCode
-    $language: LanguageCode
-    $first: Int
-    $last: Int
-    $startCursor: String
-    $endCursor: String
-  ) @inContext(country: $country, language: $language) {
-    products(first: $first, last: $last, before: $startCursor, after: $endCursor) {
-      nodes {
-        ...ProductItem
-      }
-      pageInfo {
-        hasPreviousPage
-        hasNextPage
-        startCursor
-        endCursor
-      }
+    query Catalog(
+        $country: CountryCode
+        $language: LanguageCode
+        $first: Int
+        $last: Int
+        $startCursor: String
+        $endCursor: String
+    ) @inContext(country: $country, language: $language) {
+        products(first: $first, last: $last, before: $startCursor, after: $endCursor) {
+            nodes {
+                ...ProductItem
+            }
+            pageInfo {
+                hasPreviousPage
+                hasNextPage
+                startCursor
+                endCursor
+            }
+        }
     }
-  }
   ${PRODUCT_ITEM_FRAGMENT}
 ` as const
